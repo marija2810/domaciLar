bdcea5460d38b1d62085107f7df9bcda727fe0fa
Marija Djokic <djokicmarija2810@gmail.com>
2022-04-30 11:12:10 +0200

    povezivanje modela

--- aplikacija/app/Models/Book.php
+++ aplikacija/app/Models/Book.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable=['title','description','genre_id','writer_id','pages','img'];
+
+    public function writer(){
+        return $this->belongsTo(Writer::class);
+    }
+    public function genre(){
+        return $this->belongsTo(Genre::class);
+    }
 }
