d77594813f5f31b392f6f3e2efdc41b35f8370d4
Marija Djokic <djokicmarija2810@gmail.com>
2022-04-30 13:20:48 +0200

    Napravljen factory

---
+++ aplikacija/database/factories/WriterFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Writer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Writer>
+ */
+class WriterFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Writer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name'=>$this->faker->firstName(),
+            'last_name'=>$this->faker->lastName
+        ];
+    }
+}
