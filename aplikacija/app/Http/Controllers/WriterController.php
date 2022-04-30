75ec18a1e2d2a98b17e7f6935964538d75889e52
Marija Djokic <djokicmarija2810@gmail.com>
2022-04-30 23:37:17 +0200

    ispravljanje gresaka

--- aplikacija/app/Http/Controllers/WriterController.php
+++ aplikacija/app/Http/Controllers/WriterController.php
@@ -19,16 +19,6 @@ class WriterController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -50,17 +40,6 @@ class WriterController extends Controller
     public function show(Writer $writer)
     {
         return response()->json($writer,200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Writer  $writer
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Writer $writer)
-    {
-        //
     }
 
     /**
