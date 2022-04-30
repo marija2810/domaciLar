46dcc16a2aaaca448431bcc2f2ed718d35f186ad
Marija Djokic <djokicmarija2810@gmail.com>
2022-04-30 11:22:46 +0200

    dodati spoljni kljucevi u migraciji

--- aplikacija/database/migrations/2022_04_29_203057_update_books.php
+++ aplikacija/database/migrations/2022_04_29_203057_update_books.php
@@ -13,7 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('books', function (Blueprint $table) {
+            $table->foreignId('writer_id')->constrained();
+            $table->foreignId('genre_id')->constrained();
+        });
     }
 
     /**
@@ -23,6 +26,9 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('books', function (Blueprint $table) {
+            $table->removeColumn('genre_id');
+            $table->removeColumn('writer_id');
+        });
     }
 };
