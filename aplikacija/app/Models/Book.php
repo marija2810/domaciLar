845b5ce254538ec9e1abc5965601eb45e7fa6081
Marija Djokic <djokicmarija2810@gmail.com>
2022-04-30 12:26:56 +0200

    ispravljena greska-migracije

--- aplikacija/app/Models/Book.php
+++ aplikacija/app/Models/Book.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
-
     protected $fillable=['title','description','genre_id','writer_id','pages','img'];
 
     public function writer(){
--- aplikacija/app/Models/Genre.php
+++ aplikacija/app/Models/Genre.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     use HasFactory;
-
+    
     protected $fillable=['value'];
 }
